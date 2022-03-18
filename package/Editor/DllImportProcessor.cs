6f8acb1d35ee1cbd7acb5833255220ca79f6b27e
Marcel Wiessler <marcel@gaisterhand.de>
2022-03-18 15:38:08 +0100

    Automatically add roslyn analyzer label

---
+++ package/Editor/DllImportProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace UnityAnalyzers.Core
+{ 
+	public class DllImportProcessor : AssetPostprocessor
+	{
+		
+		private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+		{
+			foreach (var imp in importedAssets)
+			{
+				if (imp.EndsWith(".dll", StringComparison.Ordinal) && imp.StartsWith("Packages/com.needle.analyzers/"))
+				{
+					var dll = AssetDatabase.LoadAssetAtPath<Object>(imp);
+					var labels = AssetDatabase.GetLabels(dll);
+					if (!labels.Contains("RoslynAnalyzer"))
+					{
+						Debug.Log("Add Analyzer label to " + dll);
+						var list = labels.ToList();
+						list.Add("RoslynAnalyzer");
+						labels = list.ToArray();
+						AssetDatabase.SetLabels(dll, labels);
+					}
+				}
+			}
+		}
+	}
+}
